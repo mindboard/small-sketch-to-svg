040da971151cbb68d15bf902e9d737303858f8b5
Tomoaki Oshima <tomo.osima@gmail.com>
2019-08-15 10:26:59 +0900

    Add rendering hint for jpegtranscoder.

--- src/main/java/com/mindboardapps/app/smallsketch/tosvg/SsfToJpeg.kt
+++ src/main/java/com/mindboardapps/app/smallsketch/tosvg/SsfToJpeg.kt
@@ -5,9 +5,13 @@ import org.apache.batik.transcoder.image.JPEGTranscoder
 import com.mindboardapps.app.smallsketch.tosvg.style.IStyleObject
 
 class SsfToJpeg(styleObject: IStyleObject) : AbstractSsfToImg(styleObject), ISsfToImg {
-	private val transcoder = JPEGTranscoder()
+    private val transcoder: JPEGTranscoder
+    init {
+        transcoder = JPEGTranscoder()
+        transcoder.addTranscodingHint(JPEGTranscoder.KEY_QUALITY, 0.9f)
+    }
 
     override fun createImage(lines: List<String>, outputStream: OutputStream){
-		super.createImage(transcoder, lines, outputStream)
+        super.createImage(transcoder, lines, outputStream)
     }
 }
